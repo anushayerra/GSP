6b5c781dc8ab9deb1f398b969f5dde57eff2cad5
Anusha Yerra -X (ayerra - SRS CONSULTING INC at Cisco) <ayerra@cisco.com>
2018-09-25 12:30:07 +0530

    GSP Project Created

---
+++ src/main/java/com/srs/gsp/App.java
@@ -0,0 +1,13 @@
+package com.srs.gsp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
